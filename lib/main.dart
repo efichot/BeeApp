8dd518520e84e1a654a9d65a7540cb41085ff72d
efichot <fichotetienne@gmail.com>
2019-11-07 16:34:46 +0700

    FCM web && mobile working ✅

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:bee_app/screens/sign_in/sign_in.dart';
 import 'package:bee_app/screens/sign_up/sign_up.dart';
 import 'package:bee_app/screens/success_send/success_send.dart';
 import 'package:bee_app/screens/success_sign_in/success_sign_in.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +15,38 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+    void iosPermission() {
+      _firebaseMessaging.requestNotificationPermissions(
+          IosNotificationSettings(sound: true, badge: true, alert: true));
+      _firebaseMessaging.onIosSettingsRegistered
+          .listen((IosNotificationSettings settings) {
+        print("Settings registered: $settings");
+      });
+    }
+
+    if (Theme.of(context).platform == TargetPlatform.iOS) iosPermission();
+
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('on message $message');
+        print(message["notification"]["title"]);
+        final snackBar =
+            SnackBar(content: Text(message["notification"]["title"]));
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
+      },
+    );
+
     return MultiProvider(
       providers: [
         Provider<bool>.value(value: identical(0, 0.0)),
