a314ed80fb7a4223a5737188812d2defe182e090
efichot <fichotetienne@gmail.com>
2019-11-17 16:06:40 +0700

    SignIn with google web and mobile ✅

--- lib/services/authWeb.dart
+++ lib/services/authWeb.dart
@@ -1,6 +1,5 @@
 import 'package:bee_app/models/user.dart';
 import 'package:firebase/firebase.dart';
-import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
@@ -34,28 +33,9 @@ class AuthService {
   }
 
   Future<InfoUser> signInWithGoogle() async {
-    final GoogleSignIn googleSignIn = GoogleSignIn();
-    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
-
-    if (googleUser != null) {
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
-      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
-        final UserCredential authResult =
-            await auth().signInWithCredential(GoogleAuthProvider.credential(
-          googleAuth.idToken,
-          googleAuth.accessToken,
-        ));
-        return userFromFirebase(authResult.user);
-      } else {
-        throw PlatformException(
-            code: 'ERROR_MISSING_GOOGLE_AUTH_TOKEN',
-            message: 'Missing Google Auth Token');
-      }
-    } else {
-      throw PlatformException(
-          code: 'ERROR_ABORTED_BY_USER', message: 'Sign in aborted by user');
-    }
+    GoogleAuthProvider provider = GoogleAuthProvider();
+    UserCredential authResult = await auth().signInWithPopup(provider);
+    return userFromFirebase(authResult.user);
   }
 
   InfoUser currentUser() {
